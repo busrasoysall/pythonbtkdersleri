5da91df8e18426e0a1d72a4bd52042f6385d7c6b
Büşra Soysal <soysallbusra@gmail.com>
2025-02-01 13:07:16 +0300

    python

---
+++ Yazılı Ortalaması Hesaplama/main.py
@@ -0,0 +1,8 @@
+not1=float(input("vize : "))
+not2=float(input("final : "))
+ortalama=(not1+not2)/2
+print("ortalamanız :", ortalama)
+if(ortalama<50):
+    print("Kaldınız :( ")
+else:
+    print ("Tebrikler Geçtiniz")
